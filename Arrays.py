966d8ca74cd5a8d66c237ac4966b37353d964024
Ajaz <41190679+Ajazzz@users.noreply.github.com>
2023-04-27 06:40:52 +0530

    Update Arrays.py

--- Arrays.py
+++ Arrays.py
@@ -36,7 +36,7 @@ print(new_arr)   # Output: [[1 2 3], [4 5 6]]
 
 
 ''' Count Occurrences '''
-''' returns the number of times the element x appears in the given array using the count method.
+ returns the number of times the element x appears in the given array using the count method.
  here are sone examples; '''
 
 def count_occurrences(arr, x):
